2467913115c8458333150d58675670599ddf428a
Amlac Eniotna <antoine.calma30@gmail.com>
2025-04-12 16:02:06 +0200

    bg init

---
+++ app/layout.tsx
@@ -0,0 +1,25 @@
+import type { Metadata } from "next";
+import { Nunito_Sans } from "next/font/google";
+import "./globals.css";
+
+const nunitoSans = Nunito_Sans({
+  variable: "--font-nunito-sans",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "BringWhat",
+  description: "Liste entre ami",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={`${nunitoSans.variable} antialiased`}>{children}</body>
+    </html>
+  );
+}
